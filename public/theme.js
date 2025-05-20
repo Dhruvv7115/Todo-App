3cc45064bd57c223609ef688b26c119099c35cfd
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2025-05-21 01:37:06 +0530

    add dark and light mode using tailwind manual plus based on device preference

---
+++ public/theme.js
@@ -0,0 +1,46 @@
+//icons
+const sunIcon = document.querySelector("#sun");
+const moonIcon = document.querySelector("#moon");
+
+//theme vars
+const userTheme = localStorage.getItem("theme")
+const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+//icon toggling
+const iconToggle = () => {
+  moonIcon.classList.toggle("hidden")
+  sunIcon.classList.toggle("hidden")
+}
+
+//initial theme-check
+const themeCheck = () => {
+  if(userTheme === "dark"|| (!userTheme && systemTheme)){
+    document.documentElement.classList.add("dark");
+    moonIcon.classList.add("hidden");
+    return;
+  }
+  sunIcon.classList.add("hidden")
+}
+
+//manual theme
+const themeSwitch = () => {
+  iconToggle();
+  if(document.documentElement.classList.contains("dark")){
+    document.documentElement.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+    return;
+  }
+  document.documentElement.classList.add("dark");
+  localStorage.setItem("theme", "dark");
+}
+
+//call theme switch on clicking buttons
+const themeIcons = document.querySelectorAll(".theme-icon");
+themeIcons.forEach(themeIcon => {
+  themeIcon.addEventListener('click', function(){
+    themeSwitch();
+  })
+})
+
+//invoke theme check on intial load
+themeCheck();
